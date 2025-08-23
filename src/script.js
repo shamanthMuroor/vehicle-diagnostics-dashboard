636ddbd1e380554bf9e2caf825c456bb5d45cca9
shamanthMuroor <shamanthm3@gmail.com>
2025-08-24 00:05:45 +0200

    Boilerplate HTML, generate random speed fn

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-//
+//select elements from DOM
+const button = document.getElementById("generateSpeed");
+const speedDisplay = document.getElementById("speedValue");
+
+// Function to generate random speed (0-350 km/h)
+function generateRandomSpeed() {
+    return Math.floor(Math.random() * 351); // 0 - 350 speed
+}
+
+button.addEventListener("click", () => {
+    const speed = generateRandomSpeed();
+    speedDisplay.textContent = speed;
+});
